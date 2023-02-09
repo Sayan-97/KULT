ee3293025ecd00858d2bb2244ad16308c1b5ac5b
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2023-02-09 18:20:04 +0530

    updated the sections

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Home from './components/Home/Home';
 import IGO from './components/IGO/IGO';
 import KultOffers from './components/KultOffers/KultOffers';
 import Navbar from './components/Navbar/Navbar';
+import Mountains from './utils/Mountains/Mountains';
+import LinesBg from './utils/LinesBg/LinesBg'
+import Gradient from './utils/Gradient/Gradient';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <KultOffers />
-      <IGO />
+      <Mountains />
+      <LinesBg />
+      <Gradient />
+      <main>
+        <Home />
+        <KultOffers />
+        <IGO />
+      </main>
     </>
   );
 }
