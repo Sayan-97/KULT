92e810ef2052bce6d51acb9493ca4d56f5a9e5ec
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2023-02-15 04:51:25 +0530

    added new navbar and navbar effects

--- src/utils/Navbar/Navbar.jsx
+++ src/utils/Navbar/Navbar.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Navbar.css'
 import navLogo from '../../images/Logo.png'
 
 import { motion } from 'framer-motion'
 
 const Navbar = () => {
+
+    const [show, setShow] = useState(true);
+    const [lastScrollTop, setLastScrollTop] = useState(0);
+    const [navbarColor, setNavbarColor] = useState(false)
+
+    const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const navbar = document.querySelector("nav");
+        const navbarHeight = navbar.offsetHeight;
+        if (currentScrollPos > lastScrollTop && currentScrollPos > navbarHeight) {
+            setShow(false);
+        } else {
+            setShow(true);
+        }
+
+        setLastScrollTop(currentScrollPos <= 0 ? 0 : currentScrollPos);
+
+        if (currentScrollPos > navbarHeight) {
+            setNavbarColor(true);
+        } else {
+            setNavbarColor(false);
+        }
+
+        setLastScrollTop(currentScrollPos <= 0 ? 0 : currentScrollPos);
+    };
+
+    useEffect(() => {
+        // Check navbar color on initial load
+        const navbar = document.querySelector('nav');
+        const navbarHeight = navbar.offsetTop;
+        if (window.pageYOffset > navbarHeight) {
+            setNavbarColor(true);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollTop]);
+
     return (
         <motion.nav
-            initial = {{ y:-100, opacity: 0 }}
-            animate = {{ y: 0, opacity: 1 }}
-            transition = {{ duration: 1 }}
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: show ? 0 : -100, opacity: 1 }}
+            transition={{ duration: 1, ease: 'easeInOut' }}
+            className = {`navbar ${navbarColor ? 'scrolled' : ''}`}
         >
             <img className='nav__logo' src={navLogo} alt="nav-logo"></img>
             <ul className="nav__menu">
